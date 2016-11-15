ef05e6a3f0ecb03deee799f7993f41cb0549f64e
Bubbles432 <CONG1_15@uni.worc.ac.uk>
2016-11-15 10:28:35 +0000

    hghjb

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
+$(document).on("pagecreate","#pageone",function(){
+                        
+	$('#taptext').on("tap",function(){
+		$(this).css('color', 'red');
+ 	}); 
+
+	function random() {
+		return !Math.round(Math.random)
+	}
+	
+});
